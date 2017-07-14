9d19378767d39f22fd378b74f99d751f9afd9b15
Abby Lambert <abby.lambert7899@gmail.com>
2017-07-14 17:19:32 -0500

    almost finished script

--- parse_log.py
+++ parse_log.py
@@ -6,18 +6,55 @@ log = log_file.read()
 
 delete_line = False
 
-output = ""
+output1 = ""
 
 for c in log:
     
     if "\n" in c: 
         delete_line = False
-	print "Starting a new line!"
+
     if delete_line == False:
         if "*" in c:
             delete_line = True
         else:
-            output = output+c
+            output1 = output1+c
 
-print output
+
+
+print output1
+output2 = ""
+delete_date = False
+
+for c in output1:
+    if " " in c:
+       if delete_date == True:
+	   delete_date = False
+       else:
+           output2 = output2+","
+
+    else:
+        if delete_date == False:
+            if "\n" in c:
+	        delete_date = True
+	    output2 = output2+c    
            
+
+print output2
+
+host1 = ""
+host2 = ""
+
+count = 0
+even = True
+
+for c in output2:
+    if even == True:
+        host1 = host1+c
+    else:
+        host2 = host2+c
+    if "\n" in c:
+        even = not even
+        
+
+print host1
+print host2 
